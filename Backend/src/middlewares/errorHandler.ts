a356b593390ed7319b91969d5c940d123cc01a77
Dyvd4 <david.kimmich@gmx.net>
2022-08-18 19:28:19 +0200

    Update errorHandler.ts

--- Backend/src/middlewares/errorHandler.ts
+++ Backend/src/middlewares/errorHandler.ts
@@ -1,9 +1,10 @@
 import logger from "../services/logger";
 
-export const errorHandler = (err, req, res, next) => {
+export const errorHandler = (err: Error, req, res, next) => {
     logger.error({
         message: "Unhandled exception / rejection",
-        error: String(err)
+        error: err.message,
+        stack: err.stack
     });
     res.status(500).json({
         error: String(err)
