831fb0dfc7b821946e1ac8b8263600707c816b23
Dyvd4 <david.kimmich@gmx.net>
2022-08-27 17:27:32 +0200

    Rework errorHandling
    -- also added notes

--- Client/src/utils/error.ts
+++ Client/src/utils/error.ts
@@ -14,20 +14,40 @@ export type ValidationErrorMap = {
     [key: string]: string
 }
 
-export function getValidationErrorMap({ response: { data: { validation } } }): ValidationErrorMap {
-    const validationErrorMap = validation.error?.details?.reduce((map, detail, index) => {
+export function getValidationErrorMap({ response: { data: { validation, validations } } }): ValidationErrorMap {
+    let errorDetails: any[] = [];
+    const validationsToMap = validation
+        ? [validation || {}]
+        : validations;
+
+    validationsToMap.forEach(validation => {
+        if (validation?.error?.details) {
+            errorDetails = errorDetails.concat(validation.error.details);
+        }
+    });
+    const validationErrorMap = errorDetails.reduce((map, detail, index) => {
         if (index === 0) {
             map = {}
         }
         map[detail.context.key] = detail.message
         return map;
     }, {}) || {};
-    if (validation.message) validationErrorMap["Fieldless"] = validation.message;
+    if (validation?.message) validationErrorMap["Fieldless"] = validation.message;
     return validationErrorMap;
 }
 
-export function getValidationError({ response: { data: { validation } } }) {
-    let validationError = validation.message ||
-        validation.error?.details?.map(detail => detail.message)
-    return validationError;
+export function getValidationErrorMessages({ response: { data: { validation, validations } } }) {
+    const mapErrorMessages = (validation) => validation.error.details.map(detail => detail.message)
+    let errorMessages: any[] = [];
+    const validationsToMap = validation
+        ? [validation || {}]
+        : validations;
+
+    validationsToMap.forEach(validation => {
+        if (validation?.message) errorMessages.push(validation.message);
+        if (validation?.error?.details) {
+            errorMessages = errorMessages.concat(mapErrorMessages(validation));
+        }
+    });
+    return errorMessages;
 }
