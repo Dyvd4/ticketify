18340e965cbdff8f57c355694e9853b548ec2b3f
Dyvd4 <david.kimmich@gmx.net>
2022-08-29 00:26:34 +0200

    Removed create action for listItem
    -- also added padding for editor and utils

---
+++ Client/src/utils/draftJs.ts
@@ -0,0 +1,5 @@
+import { ContentState, EditorState } from "draft-js";
+
+export const getEmptyState = (editorState: EditorState) => {
+    return EditorState.push(editorState, ContentState.createFromText(""), "remove-range");
+}
