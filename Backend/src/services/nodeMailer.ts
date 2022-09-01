20ab87b541ce2306ac6cf64ed77878f373bf3646
Dyvd4 <david.kimmich@gmx.net>
2022-09-01 16:13:59 +0200

    Added nodemailer

---
+++ Backend/src/services/nodeMailer.ts
@@ -0,0 +1,17 @@
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.join(__dirname, "../../.env") });
+
+const username = process.env.SMTP_USERNAME;
+const password = process.env.SMTP_PASSWORD;
+
+export const transporter = nodemailer.createTransport({
+    host: "smtp.mailgun.org",
+    port: 587,
+    secure: false,
+    auth: {
+        user: username,
+        pass: password
+    }
+});
