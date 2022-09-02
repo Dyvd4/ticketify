5e8c60ffa521da8ed48ce7bac81d3287cdea6cf6
Dyvd4 <david.kimmich@gmx.net>
2022-09-02 14:49:32 +0200

    Heart button refactoring
    -- moved state up

--- Client/src/components/Buttons/Heart.tsx
+++ Client/src/components/Buttons/Heart.tsx
@@ -4,15 +4,13 @@ import { faHeart } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import * as React from 'react';
 
-interface HeartProps {
-    color: Tailwind.Color
-    onClick?(...args): void
-    size: SizeProp
-}
+type HeartProps = {
+    color?: Tailwind.Color
+    size?: SizeProp
+    active?: boolean
+} & React.ComponentPropsWithRef<"div">
 
-interface HeartState {
-
-}
+interface HeartState { }
 
 interface Heart {
     heartRef: React.RefObject<HTMLDivElement>
@@ -20,47 +18,41 @@ interface Heart {
 
 class Heart extends React.Component<HeartProps, HeartState> {
     state = {
-        active: false,
         mouseDown: false
     }
     constructor(props) {
         super(props);
         this.heartRef = React.createRef();
     }
-    static defaultProps: HeartProps = {
-        color: {
-            value: "text-gray-400",
-            hover: "text-gray-300"
-        },
-        size: "1x"
-    }
-    handleClick = (active: boolean) => {
-        let heart = this.heartRef.current;
-        if (heart) {
-            heart.classList.remove(active ? "heart-button--unmount" : "heart-button--mount");
-            heart.classList.add(active ? "heart-button--mount" : "heart-button--unmount");
-        }
-        this.setState({ active });
-        if (this.props.onClick) this.props.onClick(this.state.active);
-    }
     render() {
-        const { mouseDown, active } = this.state;
-        const { color, size } = this.props;
+        const { mouseDown } = this.state;
+        const {
+            color = {
+                value: "text-gray-400",
+                hover: "text-gray-300"
+            },
+            size = "1x",
+            active
+        } = this.props;
         return (
-            <div ref={this.heartRef} className="inline-block relative">
+            <div
+                ref={this.heartRef}
+                className="inline-block relative"
+                {...this.props}>
                 <div className="heart-button-circles"></div>
                 <FontAwesomeIcon
                     onMouseDown={() => { this.setState({ mouseDown: true }) }}
                     onMouseUp={() => { this.setState({ mouseDown: false }) }}
                     onMouseLeave={() => { this.setState({ mouseDown: false }) }}
-                    onClick={() => { this.handleClick(!this.state.active) }}
                     className={`transform
                     ${active ? "text-green-400" : color.value} 
                     ${!active ? `hover:${color.hover}` : ""}
                     active:${color.value} active:scale-90
+                    ${active ? "heart-button--mount" : "heart-button--unmount"}
                     cursor-pointer`}
                     icon={(mouseDown || active) ? faHeart : farHeart}
-                    size={size} />
+                    size={size}
+                />
             </div>
         );
     }
