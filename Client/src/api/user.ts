5d60af45aa8faee854d38bc72d22d67528a5402b
Dyvd4 <david.kimmich@gmx.net>
2022-09-16 23:57:53 +0200

    Update user.ts

--- Client/src/api/user.ts
+++ Client/src/api/user.ts
@@ -1,6 +1,10 @@
 import { request } from "../services/request";
 
-const myRequest = request();
+const validStatuses = [200, 401]
+
+const myRequest = request({
+    validateStatus: (status) => validStatuses.includes(status)
+});
 
 export async function fetchUser() {
     let user: any = null;
@@ -9,7 +13,7 @@ export async function fetchUser() {
     return { user };
 }
 
-export async function fetchUserAll(){
+export async function fetchUserAll() {
     let user: any = null;
     const response = await myRequest.get("user/all");
     if (response.status === 200) user = response.data;
