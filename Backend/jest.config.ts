6157649d4dca5d5922e467136e8a2c80ded365e9
Dyvd4 <david.kimmich@gmx.net>
2022-12-16 18:57:45 +0100

    Fixed jest setup
    -- included path mapping
    -- removed comments from tsconfig.json as it failed to parse when importing it
    - switched to ts-jest instead of babel

---
+++ Backend/jest.config.ts
@@ -0,0 +1,12 @@
+import { JestConfigWithTsJest } from "ts-jest"
+import { pathsToModuleNameMapper } from "ts-jest"
+import { compilerOptions } from "./tsconfig.json"
+
+const config: JestConfigWithTsJest = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ["<rootDir>"],
+  modulePaths: [compilerOptions.baseUrl],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths)
+}
+export default config
