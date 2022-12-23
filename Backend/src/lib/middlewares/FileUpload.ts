895e97b3c047a37a2352cc330b4228ab95f47f6b
Dyvd4 <david.kimmich@gmx.net>
2022-12-23 14:06:06 +0100

    Removed custom middleware for image validation
    -- replaced it with multer's built in fileFilter

--- Backend/src/lib/middlewares/FileUpload.ts
+++ Backend/src/lib/middlewares/FileUpload.ts
@@ -1,21 +1,41 @@
 import config from "@config";
 import { isImageFile } from "@lib/utils/FileUtils";
-import multer from "multer";
+import { Request } from "express";
+import multer, { FileFilterCallback } from "multer";
 
 const { FILE_IMAGE_MAX_SIZE_KB, FILE_IMAGE_MAX_COUNT, FILE_MAX_COUNT, FILE_MAX_SIZE_KB, FILE_UPLOAD_PATH } = config;
 const FILE_IMAGE_MAX_SIZE_B = FILE_IMAGE_MAX_SIZE_KB * 1000;
 const FILE_MAX_SIZE_B = FILE_MAX_SIZE_KB * 1000;
 
-export const imageUpload = multer({
+const imageFileFilter = (req: Request, file: Express.Multer.File, cb: FileFilterCallback) => {
+    const error = !isImageFile(file)
+        ? new Error(`MulterError: FileUpload only supports the following fileType regex: ${config.VALID_IMAGETYPES_REGEX}`)
+        : null;
+    if (error) cb(error);
+    else cb(null, true);
+}
+
+export const multipleImageUpload = multer({
     limits: {
         fileSize: FILE_IMAGE_MAX_SIZE_B
     },
     storage: multer.diskStorage({
         destination: FILE_UPLOAD_PATH
-    })
+    }),
+    fileFilter: imageFileFilter
 }).array("files", FILE_IMAGE_MAX_COUNT);
 
-export const fileUpload = multer({
+export const singleImageUpload = multer({
+    limits: {
+        fileSize: FILE_IMAGE_MAX_SIZE_B
+    },
+    storage: multer.diskStorage({
+        destination: FILE_UPLOAD_PATH
+    }),
+    fileFilter: imageFileFilter,
+}).single("file");
+
+export const multipleFileUpload = multer({
     limits: {
         fileSize: FILE_MAX_SIZE_B
     },
@@ -24,30 +44,18 @@ export const fileUpload = multer({
     })
 }).array("files", FILE_MAX_COUNT);
 
-export const validateFiles = (req, res, next) => {
-    next();
-}
-
-export const validateImageFiles = (req, res, next) => {
-    const files = req.files;
-    let error: Error | undefined;
-    for (const file of files) {
-        if (!isImageFile(file)) {
-            error = new Error(`MulterError: FileUpload only supports the following fileType regex: ${config.VALID_IMAGETYPES_REGEX}`);
-        }
-        if (error) break;
-    }
-    if (error) {
-        return res.status(500).json({
-            error: String(error)
-        });
-    }
-    next();
-}
+export const singleFileUpload = multer({
+    limits: {
+        fileSize: FILE_MAX_SIZE_B
+    },
+    storage: multer.diskStorage({
+        destination: FILE_UPLOAD_PATH
+    })
+}).single("file");
 
 export default {
-    imageUpload,
-    fileUpload,
-    validateImageFiles,
-    validateFiles
+    multipleImageUpload,
+    multipleFileUpload,
+    singleFileUpload,
+    singleImageUpload
 }
