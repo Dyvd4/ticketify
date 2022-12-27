98db509eb49d60afdb6ce3dcfa05547f1234be69
Dyvd4 <david.kimmich@gmx.net>
2022-12-27 12:36:21 +0100

    Added ref prop to formcontrol

--- Client/src/components/Wrapper/FormControl.tsx
+++ Client/src/components/Wrapper/FormControl.tsx
@@ -1,8 +1,12 @@
 import { FormControl as ChakraFormControl, FormControlProps as ChakraFormControlProps, FormErrorMessage } from "@chakra-ui/react";
+import { ComponentPropsWithRef } from "react";
 
 type FormControlProps = {
     errorMessage?: string
 } & ChakraFormControlProps
+    // only picked specific props from ComponentPropsWithRef
+    // bc ChakraFormControlProps is already there and should provide all the other html props
+    & Pick<ComponentPropsWithRef<"div">, "ref">
 
 function FormControl({ errorMessage, children, ...props }: FormControlProps) {
     return (
