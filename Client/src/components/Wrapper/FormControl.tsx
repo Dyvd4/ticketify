8cff133bdccc2d38f5f84a15924531b7a005a6f3
Dyvd4 <david.kimmich@gmx.net>
2022-12-27 12:39:27 +0100

    reverted adding props as ref seems to not be forwarded

--- Client/src/components/Wrapper/FormControl.tsx
+++ Client/src/components/Wrapper/FormControl.tsx
@@ -1,12 +1,8 @@
 import { FormControl as ChakraFormControl, FormControlProps as ChakraFormControlProps, FormErrorMessage } from "@chakra-ui/react";
-import { ComponentPropsWithRef } from "react";
 
 type FormControlProps = {
     errorMessage?: string
 } & ChakraFormControlProps
-    // only picked specific props from ComponentPropsWithRef
-    // bc ChakraFormControlProps is already there and should provide all the other html props
-    & Pick<ComponentPropsWithRef<"div">, "ref">
 
 function FormControl({ errorMessage, children, ...props }: FormControlProps) {
     return (
