6ba4b3c1651a0c94418c5be8e741db038eb0df44
Dyvd4 <david.kimmich@gmx.net>
2023-02-11 19:37:14 +0100

    Added loading border

---
+++ Client/src/components/LoadingBorder/LoadingBorder.tsx
@@ -0,0 +1,37 @@
+import { ComponentPropsWithRef, PropsWithChildren, useEffect } from 'react';
+import "./LoadingBorder.styles.scss";
+
+type _LoadingBorderProps = {
+    animationDurationMs?: number
+}
+
+export type LoadingBorderProps = PropsWithChildren<_LoadingBorderProps> &
+    Omit<ComponentPropsWithRef<'div'>, keyof _LoadingBorderProps>
+
+function LoadingBorder({ className, animationDurationMs, ...props }: LoadingBorderProps) {
+
+    useEffect(() => {
+        if (animationDurationMs) {
+            document.documentElement.style.setProperty("--loading-border-animation-durationMs", animationDurationMs + "ms")
+        }
+    }, []);
+
+    return (
+        <div
+            id="test"
+            className={`relative loading-border overflow-hidden ${className}`}
+            {...props}>
+            <div className="w-full h-1
+                            bg-gradient-to-r from-transparent to-cyan-500"></div>
+            <div className="h-full w-1
+                            bg-gradient-to-b from-transparent to-cyan-500"></div>
+            <div className="w-full h-1
+                            bg-gradient-to-l from-transparent to-cyan-500"></div>
+            <div className="h-full w-1
+                            bg-gradient-to-t from-transparent to-cyan-500"></div>
+            {props.children}
+        </div>
+    );
+}
+
+export default LoadingBorder;
