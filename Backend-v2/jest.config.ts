b6148e19f751889c2df02af2c7131d311ed09619
Dyvd4 <david.kimmich@gmx.net>
2023-02-26 13:17:17 +0100

    setup e2e tests

---
+++ Backend-v2/jest.config.ts
@@ -0,0 +1,25 @@
+import { JestConfigWithTsJest } from "ts-jest"
+import { pathsToModuleNameMapper } from "ts-jest"
+import { compilerOptions } from "./tsconfig.json"
+
+const config: JestConfigWithTsJest = {
+	preset: 'ts-jest',
+	testEnvironment: 'node',
+	roots: ["<rootDir>"],
+	modulePaths: [compilerOptions.baseUrl],
+	moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+	moduleFileExtensions: [
+		"js",
+		"json",
+		"ts"
+	],
+	testRegex: ".*\\.spec\\.ts$",
+	transform: {
+		"^.+\\.(t|j)s$": "ts-jest"
+	},
+	collectCoverageFrom: [
+		"**/*.(t|j)s"
+	],
+	coverageDirectory: "../coverage"
+}
+export default config
