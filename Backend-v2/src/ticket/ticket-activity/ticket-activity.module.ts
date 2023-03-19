e1e845bdb179f0c1566f6c66901e23b2f2fdd12d
Dyvd4 <david.kimmich@gmx.net>
2023-03-19 17:26:30 +0100

    Update ticket-activity.module.ts

--- Backend-v2/src/ticket/ticket-activity/ticket-activity.module.ts
+++ Backend-v2/src/ticket/ticket-activity/ticket-activity.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TicketActivityController } from './ticket-activity.controller';
 
 @Module({
-  controllers: [TicketActivityController],
-  providers: []
+	controllers: [TicketActivityController],
+	providers: []
 })
-export class TicketActivityModule {}
+export class TicketActivityModule { }
