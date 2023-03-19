097ff911062176ea4d7dcdba949d74363f0d51a9
Dyvd4 <david.kimmich@gmx.net>
2023-03-19 14:03:33 +0100

    Update app.module.ts

--- Backend-v2/src/app.module.ts
+++ Backend-v2/src/app.module.ts
@@ -3,9 +3,19 @@ import { CommentModule } from './comment/comment.module';
 import { GlobalModule } from './global/global.module';
 import { TestController } from './test/test.controller';
 import { UserModule } from './user/user.module';
+import { CommentInteractionModule } from './comment-interaction/comment-interaction.module';
+import { TicketActivityModule } from './ticket-activity/ticket-activity.module';
+import { TicketModule } from './ticket/ticket.module';
 
 @Module({
-	imports: [GlobalModule, UserModule, CommentModule],
+	imports: [
+		GlobalModule,
+		UserModule,
+		CommentModule,
+		CommentInteractionModule,
+		TicketActivityModule,
+		TicketModule
+	],
 	controllers: [TestController],
 	providers: [],
 })
