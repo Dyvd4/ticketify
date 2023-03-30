53216615a9e10a173f5239b147121240c5e5dd3a
Dyvd4 <david.kimmich@gmx.net>
2023-03-30 18:31:21 +0200

    fixed config
    -- sometimes configs weren't already loaded on startup, so accessed them manually after NestFactory.create (my theory)

--- Backend-v2/src/main.ts
+++ Backend-v2/src/main.ts
@@ -4,14 +4,11 @@ import { DocumentBuilder } from '@nestjs/swagger';
 import { SwaggerModule } from '@nestjs/swagger/dist';
 import { AppModule } from './app.module';
 import { ParseParamPipe } from './global/global.parse-param.pipe';
-import config from '@config';
-
-const { CLIENT_URL } = config();
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.enableCors({
-		origin: CLIENT_URL,
+		origin: process.env.CLIENT_URL,
 		credentials: true
 	})
 	app.setGlobalPrefix("api")
