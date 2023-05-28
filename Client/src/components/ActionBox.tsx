3425cdbf2da8cd7272183c3e38383afb61840a95
Dyvd4 <david.kimmich@gmx.net>
2023-05-28 14:58:22 +0200

    Update ActionBox.tsx

--- Client/src/components/ActionBox.tsx
+++ Client/src/components/ActionBox.tsx
@@ -1,4 +1,4 @@
-import { Heading, IconButton, Menu, MenuButton, MenuList } from "@chakra-ui/react";
+import { Heading, IconButton, IconButtonProps, Menu, MenuButton, MenuList } from "@chakra-ui/react";
 import { faEllipsis } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { cloneElement, ComponentPropsWithRef, PropsWithChildren } from "react";
@@ -8,39 +8,48 @@ type _ActionBoxProps = {
 	title?: string;
 	actions?: React.ReactElement[];
 	menuActions?: React.ReactElement[];
+	menuButtonSize?: IconButtonProps["size"];
 };
 
 export type ActionBoxProps = _ActionBoxProps &
 	Omit<PropsWithChildren<ComponentPropsWithRef<"div">>, keyof _ActionBoxProps>;
 
-function ActionBox({ className, title, children, actions, menuActions, ...props }: ActionBoxProps) {
+function ActionBox({
+	className,
+	title,
+	children,
+	actions,
+	menuActions,
+	menuButtonSize,
+	...props
+}: ActionBoxProps) {
 	return (
 		<div
 			className={cn("relative justify-between gap-4 rounded-md border p-4", className)}
 			{...props}
 		>
 			<div className="flex items-center justify-between gap-4">
-				<Heading className="text-xl">{title}</Heading>
+				<Heading className="truncate text-xl">{title}</Heading>
 				<div className="flex gap-2">
-					{menuActions && (
+					{actions && (
 						<>
-							{menuActions.map((action, index) =>
+							{actions.map((action, index) =>
 								cloneElement(action, { ...action.props, key: index })
 							)}
 						</>
 					)}
-					{actions && (
+					{menuActions && (
 						<>
 							<Menu>
 								<MenuButton
 									as={IconButton}
-									size={"xs"}
+									size={menuButtonSize || "xs"}
 									aria-label="Options"
 									icon={<FontAwesomeIcon icon={faEllipsis}></FontAwesomeIcon>}
 									variant="outline"
 								/>
 								<MenuList>
-									{actions.map((action, index) =>
+									{menuActions.map((action, index) =>
 										cloneElement(action, { ...action.props, key: index })
 									)}
 								</MenuList>
