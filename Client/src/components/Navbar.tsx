5fcb602af05ee35385e11907564035d9fcd4f2ff
Dyvd4 <david.kimmich@gmx.net>
2023-05-28 11:45:41 +0200

    style improvement navbar and sidebar

--- Client/src/components/Navbar.tsx
+++ Client/src/components/Navbar.tsx
@@ -1,13 +1,13 @@
 import {
-    Avatar,
-    Box,
-    Flex,
-    HStack,
-    Link,
-    Menu,
-    MenuButton,
-    MenuItem,
-    MenuList,
+	Avatar,
+	Box,
+	Flex,
+	HStack,
+	Link,
+	Menu,
+	MenuButton,
+	MenuItem,
+	MenuList,
 } from "@chakra-ui/react";
 import { faSignOut, faSliders, faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -21,66 +21,66 @@ import SearchBar from "./SearchBar";
 type NavbarProps = {};
 
 function Navbar(props: NavbarProps) {
-    const { currentUser, isAuthenticated } = useCurrentUserWithAuthentication({
-        includeAllEntities: true,
-    });
+	const { currentUser, isAuthenticated } = useCurrentUserWithAuthentication({
+		includeAllEntities: true,
+	});
 
-    const signOutMutation = useMutation(signOut, {
-        onSuccess: () => {
-            window.location.href = "/";
-        },
-    });
+	const signOutMutation = useMutation(signOut, {
+		onSuccess: () => {
+			window.location.href = "/";
+		},
+	});
 
-    if (!isAuthenticated) return null;
+	if (!isAuthenticated) return null;
 
-    return (
-        <Box
-            as="nav"
-            id="navbar"
-            className="z-50 inline-flex justify-between border-b-2 p-2"
-            backgroundColor={"gray.900"}
-            _light={{
-                backgroundColor: "white",
-            }}
-        >
-            <Breadcrumb />
-            <HStack gap={1}>
-                <SearchBar />
-                <DarkModeButton size="sm" aria-label="Toggle dark mode" />
-                <Menu>
-                    <MenuButton>
-                        <Flex alignItems={"center"} gap={2}>
-                            <Avatar
-                                className="ring-2 ring-sky-500"
-                                size={"sm"}
-                                name={currentUser?.username}
-                                src={currentUser?.avatar?.url}
-                            />
-                        </Flex>
-                    </MenuButton>
-                    <MenuList>
-                        <Link href="/User">
-                            <MenuItem icon={<FontAwesomeIcon icon={faUser} />}>
-                                Profile data
-                            </MenuItem>
-                        </Link>
-                        <Link href="/UserSettings">
-                            <MenuItem icon={<FontAwesomeIcon icon={faSliders} />}>
-                                Settings
-                            </MenuItem>
-                        </Link>
-                        <MenuItem
-                            color="red"
-                            onClick={() => signOutMutation.mutate()}
-                            icon={<FontAwesomeIcon icon={faSignOut} />}
-                        >
-                            sign out
-                        </MenuItem>
-                    </MenuList>
-                </Menu>
-            </HStack>
-        </Box>
-    );
+	return (
+		<Box
+			as="nav"
+			id="navbar"
+			className="z-50 inline-flex justify-between border-b p-2"
+			backgroundColor={"gray.900"}
+			_light={{
+				backgroundColor: "white",
+			}}
+		>
+			<Breadcrumb />
+			<HStack gap={1}>
+				<SearchBar />
+				<DarkModeButton size="sm" aria-label="Toggle dark mode" />
+				<Menu>
+					<MenuButton>
+						<Flex alignItems={"center"} gap={2}>
+							<Avatar
+								className="ring-2 ring-sky-500"
+								size={"sm"}
+								name={currentUser?.username}
+								src={currentUser?.avatar?.url}
+							/>
+						</Flex>
+					</MenuButton>
+					<MenuList>
+						<Link href="/User">
+							<MenuItem icon={<FontAwesomeIcon icon={faUser} />}>
+								Profile data
+							</MenuItem>
+						</Link>
+						<Link href="/UserSettings">
+							<MenuItem icon={<FontAwesomeIcon icon={faSliders} />}>
+								Settings
+							</MenuItem>
+						</Link>
+						<MenuItem
+							color="red"
+							onClick={() => signOutMutation.mutate()}
+							icon={<FontAwesomeIcon icon={faSignOut} />}
+						>
+							sign out
+						</MenuItem>
+					</MenuList>
+				</Menu>
+			</HStack>
+		</Box>
+	);
 }
 
 export default Navbar;
--- Client/src/components/Sidebar/Sidebar.tsx
+++ Client/src/components/Sidebar/Sidebar.tsx
@@ -1,10 +1,10 @@
 import { Box, Divider, Heading, Link, Tooltip } from "@chakra-ui/react";
 import {
-    faArrowLeft,
-    faArrowRight,
-    faBook,
-    faFlask,
-    faTicket,
+	faArrowLeft,
+	faArrowRight,
+	faBook,
+	faFlask,
+	faTicket,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import classNames from "classnames";
@@ -24,112 +24,112 @@ type _SidebarProps = {};
 export type SidebarProps = PropsWithChildren<_SidebarProps> & BgBoxProps;
 
 function Sidebar({ className, ...props }: SidebarProps) {
-    const path = window.location.pathname;
+	const path = window.location.pathname;
 
-    const { isAuthenticated } = useCurrentUserWithAuthentication({ includeAllEntities: true });
-    const [sidebarIsCollapsed, toggleSidebarIsCollapsed] = useSidebarToggle();
-    const sidebarListItemVariant = !sidebarIsCollapsed ? "horizontal" : "vertical";
+	const { isAuthenticated } = useCurrentUserWithAuthentication({ includeAllEntities: true });
+	const [sidebarIsCollapsed, toggleSidebarIsCollapsed] = useSidebarToggle();
+	const sidebarListItemVariant = !sidebarIsCollapsed ? "horizontal" : "vertical";
 
-    const { data: ticketCount } = useQuery(["ticketCount"], () =>
-        fetchEntity({
-            route: "tickets/count",
-        })
-    );
+	const { data: ticketCount } = useQuery(["ticketCount"], () =>
+		fetchEntity({
+			route: "tickets/count",
+		})
+	);
 
-    const { data: logCount } = useQuery(["logCount"], () =>
-        fetchEntity({
-            route: "logs/count",
-        })
-    );
+	const { data: logCount } = useQuery(["logCount"], () =>
+		fetchEntity({
+			route: "logs/count",
+		})
+	);
 
-    if (!isAuthenticated) return null;
+	if (!isAuthenticated) return null;
 
-    return (
-        <BgBox
-            backgroundColor={"gray.900"}
-            _light={{
-                backgroundColor: "white",
-            }}
-            variant="child"
-            as="ul"
-            id="sidebar"
-            className={classNames(
-                `${className} flex h-screen flex-col 
-                                    rounded-none border-r-2`,
-                {
-                    "gap-4": !sidebarIsCollapsed,
-                    "gap-6": sidebarIsCollapsed,
-                }
-            )}
-            {...props}
-        >
-            {sidebarListItemVariant === "horizontal" && (
-                <>
-                    <Heading as="h1" className="flex items-center justify-between text-xl">
-                        <Link href="/" className="no-underline">
-                            <Tooltip placement="bottom" label="Go to home">
-                                <Box {...(path === "/" ? { color: "blue.200" } : {})}>
-                                    <FontAwesomeIcon className="mr-2" icon={faTicket} />
-                                    <span>Ticketify</span>
-                                </Box>
-                            </Tooltip>
-                        </Link>
-                        <IconButton
-                            backgroundColor={"transparent"}
-                            size={"sm"}
-                            aria-label="toggle sidebar"
-                            icon={<FontAwesomeIcon icon={faArrowLeft} />}
-                            onClick={toggleSidebarIsCollapsed}
-                        ></IconButton>
-                    </Heading>
-                </>
-            )}
+	return (
+		<BgBox
+			backgroundColor={"gray.800"}
+			_light={{
+				backgroundColor: "gray.100",
+			}}
+			variant="child"
+			as="ul"
+			id="sidebar"
+			className={classNames(
+				`${className} flex h-screen flex-col 
+                                    rounded-none`,
+				{
+					"gap-4": !sidebarIsCollapsed,
+					"gap-6": sidebarIsCollapsed,
+				}
+			)}
+			{...props}
+		>
+			{sidebarListItemVariant === "horizontal" && (
+				<>
+					<Heading as="h1" className="flex items-center justify-between text-xl">
+						<Link href="/" className="no-underline">
+							<Tooltip placement="bottom" label="Go to home">
+								<Box {...(path === "/" ? { color: "blue.200" } : {})}>
+									<FontAwesomeIcon className="mr-2" icon={faTicket} />
+									<span>Ticketify</span>
+								</Box>
+							</Tooltip>
+						</Link>
+						<IconButton
+							backgroundColor={"transparent"}
+							size={"sm"}
+							aria-label="toggle sidebar"
+							icon={<FontAwesomeIcon icon={faArrowLeft} />}
+							onClick={toggleSidebarIsCollapsed}
+						></IconButton>
+					</Heading>
+				</>
+			)}
 
-            {sidebarListItemVariant === "vertical" && (
-                <>
-                    <IconButton
-                        backgroundColor={"transparent"}
-                        className="mx-auto w-fit"
-                        size={"sm"}
-                        aria-label="toggle sidebar"
-                        icon={<FontAwesomeIcon icon={faArrowRight} />}
-                        onClick={toggleSidebarIsCollapsed}
-                    ></IconButton>
-                </>
-            )}
+			{sidebarListItemVariant === "vertical" && (
+				<>
+					<IconButton
+						backgroundColor={"transparent"}
+						className="mx-auto w-fit"
+						size={"sm"}
+						aria-label="toggle sidebar"
+						icon={<FontAwesomeIcon icon={faArrowRight} />}
+						onClick={toggleSidebarIsCollapsed}
+					></IconButton>
+				</>
+			)}
 
-            <NewTicketSection />
+			<NewTicketSection />
 
-            <Divider />
+			<Divider />
 
-            <SidebarListItem
-                title="Tickets"
-                urlPath={"/Ticket"}
-                variant={sidebarListItemVariant}
-                icon={faTicket}
-                count={ticketCount}
-            />
+			<SidebarListItem
+				title="Tickets"
+				urlPath={"/Ticket"}
+				variant={sidebarListItemVariant}
+				icon={faTicket}
+				count={ticketCount}
+			/>
 
-            <SidebarListItem
-                title="Logs"
-                urlPath="/Log"
-                variant={sidebarListItemVariant}
-                icon={faBook}
-                count={logCount}
-            />
+			<SidebarListItem
+				title="Logs"
+				urlPath="/Log"
+				variant={sidebarListItemVariant}
+				icon={faBook}
+				count={logCount}
+			/>
 
-            <SidebarListItem
-                title="Test"
-                urlPath={"/Test"}
-                variant={sidebarListItemVariant}
-                icon={faFlask}
-            />
+			<SidebarListItem
+				title="Test"
+				urlPath={"/Test"}
+				variant={sidebarListItemVariant}
+				icon={faFlask}
+			/>
 
-            <Divider />
+			<Divider />
 
-            <PinnedTicketsSection sidebarListItemVariant={sidebarListItemVariant} />
-        </BgBox>
-    );
+			<PinnedTicketsSection sidebarListItemVariant={sidebarListItemVariant} />
+		</BgBox>
+	);
 }
 
 export default Sidebar;
