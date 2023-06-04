8521cb5e5828a172916ccb15d7999eb4d613c424
Dyvd4 <david.kimmich@gmx.net>
2023-06-04 13:17:16 +0200

    use TagConfirmMenu for settings status of ticket

--- Client/src/pages/Ticket/Details/components/SetStatusButton.tsx
+++ Client/src/pages/Ticket/Details/components/SetStatusButton.tsx
@@ -1,33 +1,12 @@
-import {
-	Button,
-	Menu,
-	MenuItemOption,
-	MenuList,
-	MenuOptionGroup,
-	Modal,
-	ModalBody,
-	ModalCloseButton,
-	ModalContent,
-	ModalFooter,
-	ModalHeader,
-	ModalOverlay,
-	Tag,
-	useDisclosure,
-	useToast,
-} from "@chakra-ui/react";
-import { useEffect, useState } from "react";
+import { Tag, useDisclosure, useToast } from "@chakra-ui/react";
 import { useMutation, useQuery, useQueryClient } from "react-query";
 import { useParams } from "react-router-dom";
 import { fetchEntity, updateEntity } from "src/api/entity";
-import MenuButton from "src/components/Wrapper/MenuButton";
+import TagConfirmMenu from "src/components/TagConfirmMenu";
 
 type SetStatusButtonProps = {};
 
 function SetStatusButton(props: SetStatusButtonProps) {
-	// state
-	// -----
-	const [status, setStatus] = useState<any>(null);
-
 	// hooks
 	// -----
 	const { isOpen, onOpen, onClose } = useDisclosure();
@@ -43,97 +22,60 @@ function SetStatusButton(props: SetStatusButtonProps) {
 	);
 	const { data, isLoading: ticketIsLoading } = useQuery(["ticket", id]);
 
-	const ticket = data as any;
-
 	// mutations
 	// ---------
-	const mutation = useMutation(
-		() =>
+	const mutation = useMutation<any, any, any, any>(
+		(newStatus) =>
 			updateEntity({
 				route: "ticket",
 				entityId: id,
-				payload: { statusId: status.id },
+				payload: { statusId: newStatus.id },
 			}),
 		{
 			onSuccess: async () => {
 				await queryClient.invalidateQueries(["ticket", String(id)]);
-				handleOnClose(true);
 				toast({
 					status: "success",
 					title: "successfully changed status",
 				});
+				onClose();
 			},
 		}
 	);
 
-	useEffect(() => {
-		if (ticket) setStatus(ticket.status);
-	}, [ticket]);
-
-	const handleStatusChange = (statusId) => {
-		setStatus(ticketStatusResponse.items.find((status) => status.id === statusId));
-		onOpen();
+	const getConfirmDialogText = (oldStatus, newStatus) => {
+		return (
+			<>
+				<div>This will set the status from</div>
+				<Tag colorScheme={oldStatus.color} className="mr-2">
+					{oldStatus.name}
+				</Tag>
+				<span className="mr-2">to</span>
+				<Tag colorScheme={newStatus.color} className="mr-2">
+					{newStatus.name}
+				</Tag>
+				<span>?</span>
+			</>
+		);
 	};
 
-	const handleOnClose = (isSuccess: boolean) => {
-		if (!isSuccess) setStatus(ticket.status);
-		onClose();
-	};
-
+	const ticket = data as any;
 	const isLoading = ticketIsLoading || ticketStatusIsLoading;
+	if (isLoading || !ticketStatusResponse?.items || !ticket) return null;
+
+	const status = ticket.status;
 
 	return (
-		<>
-			<Modal isOpen={isOpen} onClose={() => handleOnClose(false)}>
-				<ModalOverlay />
-				<ModalContent>
-					<ModalHeader>Are you sure?</ModalHeader>
-					<ModalCloseButton />
-					<ModalBody>This will set the status to "{status?.name}".</ModalBody>
-					<ModalFooter>
-						<Button
-							isLoading={mutation.isLoading}
-							mr={3}
-							colorScheme={"cyan"}
-							onClick={() => mutation.mutate()}
-						>
-							confirm
-						</Button>
-						<Button onClick={() => handleOnClose(false)}>cancel</Button>
-					</ModalFooter>
-				</ModalContent>
-			</Modal>
-			<Menu>
-				{!isLoading && status && (
-					<>
-						<MenuButton
-							height={"6"}
-							className="cursor-pointer text-xs"
-							as={Tag}
-							// @ts-ignore
-							colorScheme={status.color}
-						>
-							{status?.name || "none"}
-						</MenuButton>
-						<MenuList>
-							<MenuOptionGroup
-								onChange={handleStatusChange}
-								value={status.id}
-								type="radio"
-							>
-								{ticketStatusResponse.items
-									.sort((a, b) => a.priority - b.priority)
-									.map((status) => (
-										<MenuItemOption key={status.id} value={status.id}>
-											<Tag colorScheme={status.color}>{status.name}</Tag>
-										</MenuItemOption>
-									))}
-							</MenuOptionGroup>
-						</MenuList>
-					</>
-				)}
-			</Menu>
-		</>
+		<TagConfirmMenu
+			className="text-xs"
+			modalIsOpen={isOpen}
+			onModalOpen={onOpen}
+			onModalClose={onClose}
+			mutation={mutation}
+			confirmDialogBodyRenderer={getConfirmDialogText}
+			selectedMenuItem={status}
+			menuItems={ticketStatusResponse.items}
+		/>
 	);
 }
 
