1357e2bbabff131dee0f52a2bff1e415071d44f1
Dyvd4 <david.kimmich@gmx.net>
2023-07-01 10:36:02 +0200

    make sidebarlistitem better lol
    -- diplay count instead of title when vertical
    -- added state peristance through local storage

--- Client/src/components/Sidebar/SidebarListItem/SidebarListItem.tsx
+++ Client/src/components/Sidebar/SidebarListItem/SidebarListItem.tsx
@@ -1,9 +1,12 @@
-import { Box, Collapse, useDisclosure } from "@chakra-ui/react";
-import { IconProp, SizeProp } from "@fortawesome/fontawesome-svg-core";
+import { Box, Collapse, Tooltip } from "@chakra-ui/react";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
 import { faCaretUp } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import classNames from "classnames";
-import { PropsWithChildren } from "react";
+import { useAtom } from "jotai";
+import { PropsWithChildren, useId } from "react";
+import { sidebarIsCollapsedAtom } from "src/context/atoms";
+import useLocalStorage from "src/hooks/useLocalStorage";
 import { cn } from "src/utils/component";
 import BaseSidebarListItem from ".";
 import { BaseSidebarListItemProps } from "./BaseSidebarListItem";
@@ -34,58 +37,75 @@ function SidebarListItem({
 	children,
 	...props
 }: SidebarListItemProps) {
-	const { isOpen, onToggle } = useDisclosure();
+	const id = useId();
+	const [isOpen, setIsOpen] = useLocalStorage(`SidebarListItem-${id}`, false);
+	const allowExpand = isOpen && variant === "horizontal";
+	const [sidebarIsCollapsed, setSidebarIsCollapsed] = useAtom(sidebarIsCollapsedAtom);
+
+	const handleClick = () => {
+		if (!children) return;
+		setIsOpen(variant === "vertical" || !isOpen);
+		sidebarIsCollapsed && setSidebarIsCollapsed(false);
+	};
 
 	return (
 		<>
-			<BaseSidebarListItem onClick={() => !!children && onToggle()} {...props}>
+			<BaseSidebarListItem onClick={handleClick} {...props}>
 				{(isActive) => (
 					<Box
 						className={classNames("flex items-center justify-center rounded-xl", {
 							"flex-col gap-2": variant === "vertical",
 						})}
 					>
-						{props.icon && (
-							<>
-								<FontAwesomeIcon
-									icon={props.icon}
-									size={(variant === "vertical" ? "lg" : "1x") as SizeProp}
-									className="aspect-square"
-								/>
-							</>
+						{props.icon && variant === "vertical" && (
+							<Tooltip label={title} placement="right">
+								<div className="flex flex-col items-center gap-2">
+									<FontAwesomeIcon
+										icon={props.icon}
+										size={"lg"}
+										className="aspect-square"
+									/>
+									{!!count && (
+										<div className="text-secondary text-xs">{count}</div>
+									)}
+								</div>
+							</Tooltip>
 						)}
-
-						<Box
-							as="h3"
-							className={classNames(``, {
-								"font-bold": isActive,
-								"ml-6 mr-auto": variant === "horizontal",
-								"text-xs": variant === "vertical",
-							})}
-						>
-							{title}
-						</Box>
-
-						{count && variant === "horizontal" && (
+						{variant === "horizontal" && (
 							<>
-								<div className="text-secondary text-sm">{count}</div>
-							</>
-						)}
+								{props.icon && (
+									<FontAwesomeIcon
+										icon={props.icon}
+										size={"1x"}
+										className="aspect-square"
+									/>
+								)}
 
-						{!!children && (
-							<>
-								<FontAwesomeIcon
-									className={cn("transition-all", {
-										"rotate-180": isOpen,
+								<Box
+									as="h3"
+									className={classNames(`ml-6 mr-auto`, {
+										"font-bold": isActive,
 									})}
-									icon={faCaretUp}
-								/>
+								>
+									{title}
+								</Box>
+
+								{!!count && <div className="text-secondary text-sm">{count}</div>}
+
+								{!!children && (
+									<FontAwesomeIcon
+										className={cn("transition-all", {
+											"rotate-180": isOpen,
+										})}
+										icon={faCaretUp}
+									/>
+								)}
 							</>
 						)}
 					</Box>
 				)}
 			</BaseSidebarListItem>
-			<Collapse in={isOpen}>
+			<Collapse in={allowExpand}>
 				<ul className="flex flex-col gap-4 pl-4">{children}</ul>
 			</Collapse>
 		</>
