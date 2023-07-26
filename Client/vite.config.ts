db468ed8028dce920ea399cafa413b07c2817265
Dyvd4 <david.kimmich@gmx.net>
2023-07-26 17:04:32 +0200

    added prod dockerfile for client

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -7,8 +7,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	// This changes the out put dir from dist to build
-	// comment this out if that isn't relevant for your project
 	build: {
 		outDir: "build",
 	},
@@ -35,5 +33,9 @@ export default defineConfig({
 		watch: {
 			usePolling: true,
 		},
+	},
+	preview: {
+		host: "0.0.0.0",
+		port: 3000,
 	},
 });
