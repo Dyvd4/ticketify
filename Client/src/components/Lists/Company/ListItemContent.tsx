12f386b6e8890d8992096b8091f3b05792fe74fe
Dyvd4 <david.kimmich@gmx.net>
2023-08-20 14:05:52 +0200

    Update ListItemContent.tsx

--- Client/src/components/Lists/Company/ListItemContent.tsx
+++ Client/src/components/Lists/Company/ListItemContent.tsx
@@ -2,7 +2,7 @@ import { Avatar, Box, Heading, Link } from "@chakra-ui/react";
 
 function TicketListItemContent({ item }: { item }) {
 	return (
-		<Box className="flex justify-between">
+		<Box className="flex items-center gap-6">
 			<Avatar size={"lg"} name={item.name} src={item.avatar?.file?.url} />
 			<Box className="flex flex-col items-end gap-2">
 				<Link href={`/Company/Details/${item.id}`}>
